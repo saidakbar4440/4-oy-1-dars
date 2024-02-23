55a2ac8751b9b28dab7ac516702234f176771c36
saidakbar4440 <saidakbar4440@gmail.com>
2024-02-23 19:24:10 +0500

    First

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+// var a=1254
+// var b=800
+// var c=220
+// var e=3993
+
+// alert(a+b+c+e)
+
+var a=prompt('Siz kimsiz?')
+var b=prompt('Yoshingiz nechida?')
+var c=prompt('qatta yashisiz?')
+
+let e=('RAxmat')
+
+
+alert(e)
+console.log(a,b,c,e);
+
